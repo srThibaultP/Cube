5636f3cb184aed1eeb4b522cdf0774398c2d82c9
Thibault <42326001+SRThibaultP@users.noreply.github.com>
2021-01-05 09:17:45 +0100

    04/01 : MàJ après refresh des nv donnés de la bdd

--- nodejs/server.js
+++ nodejs/server.js
@@ -2,18 +2,24 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const fs = require('fs');
-
-var bdddata = "./bdd.json"
-
+const mysql = require('mysql');
 // ajout de socket.io
 const server = require('http').Server(app)
-const io = require('socket.io')(server, {
+const io = require('socket.io')(3001, {
   cors: {
   origin: "http://localhost",
   methods: ["GET", "POST"],
   credentials: true
 }
 })
+var pool = mysql.createPool({
+  host: "localhost",
+  user: "snir",
+  password: "snirpass",
+  database: "AssurVehicules"
+});
+var bdddata = "./bdd.json"
+
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
@@ -32,32 +38,42 @@ io.on('connection', (socket) =>{
 })
 
 // on change app par server
-server.listen(8080, function () {
+server.listen(8081, function () {
  console.log('Votre app est disponible sur localhost:8080 !')
 })
-// établissement de la connexion
 
+function dbupdate() {
+  pool.getConnection(function(err, connection) {
+    if (err) throw err;
+    connection.query("SELECT * FROM Modele", function (err, result, fields) {
+      if (err) throw err;
+      console.log(result);
+      bddparsed = JSON.stringify(result);
+      fs.writeFile('bdd.json', bddparsed, (err) => {
+        if (err) throw err;
+        console.log('writted');
+        connection.release();
+      })
+    })
+  })
+}
+
+
+// établissement de la connexion
 io.on('connection', (socket) =>{
    console.log(`Connecté au client ${socket.id}`)
    // émission d'un évènement
-   io.emit('news','Voici un nouvel élément envoyé par le serveur');
-   io.emit('news','bonsoir paris');
-   fs.readFile('./bdd.json', function read(err, data) {
-     if (err) {
-       throw err;
-   }
-       const content = data;
-       const parsed = JSON.parse(content);
-       console.log(content);
-       console.log(parsed);
-       io.emit('json',parsed);
-
-   });
-   io.emit('json','pas vraiment');
-   io.emit('news','not json data');
-   setTimeout(function() {
-      socket.send('4s plus tard');
-      io.emit("futurrrr");
-   }, 4000);
-
+   io.emit('news','debut');
+   dbupdate();
+     fs.readFile('./bdd.json', function read(err, data) {
+       if (err) {
+         throw err;
+     }
+         const content = data;
+         const parsed = JSON.parse(content);
+//       console.log(content);
+         console.log(parsed);
+         io.emit('json',parsed);
+   })
 })
+         //setTimeout(dbupdate, 1000);
