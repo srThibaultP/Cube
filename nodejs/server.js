7be7e4b303d4b5c4dd4e149e43cf7161b7dc045e
SRThibaultP <42326001+SRThibaultP@users.noreply.github.com>
2021-01-19 13:50:44 +0100

    19/01 : Changement du nom de la fonction d'actualisation

--- nodejs/server.js
+++ nodejs/server.js
@@ -6,7 +6,11 @@ const server = require('http').Server(express)
 const io = require('socket.io')(server, config.cors);
 var pool = mysql.createPool(config.mysql);
 
-const program = async () => {
+server.listen(config.port, function () {
+  console.log('Serveur WebSocket disponible sur localhost:' + config.port)
+})
+
+const dbevent = async () => {
   const instance = new MySQLEvents(pool, {
     startAtEnd: true
   });
@@ -18,7 +22,7 @@ const program = async () => {
     expression: config.mysql.database,
     statement: MySQLEvents.STATEMENTS.ALL,
     onEvent: e => {
-      dbupdate();
+      clientwebupdate();
     }
   })
 
@@ -27,39 +31,28 @@ const program = async () => {
 
 }
 
-program().catch(console.error);
+dbevent().catch(console.error);
 
 io.on('connection', (socket) => {
   console.log(`Connecté au client ${socket.id}`)
   io.emit('status', 'Connecté au serveur WebSocket');
-  dbupdate();
-})
-
-server.listen(config.port, function() {
-  console.log('Serveur WebSocket disponible sur localhost:' + config.port)
+  clientwebupdate();
 })
 
 
 
 
-
-
-
-function dbupdate() {
-  pool.getConnection(function(err, connection) {
+function clientwebupdate() {
+  pool.getConnection(function (err, connection) {
     if (err) throw err; //Pas de connection
-
     connection.query("SELECT * FROM " + config.mysqlTable + " ORDER BY " + config.mysqlId + " DESC LIMIT 1", function (err, pos) {
-      if (err) throw err;
       io.emit('mysqlPos', pos);
       console.log(pos);
     });
-
-    connection.query("SELECT * FROM " + config.mysqlTable, function(err, result) {
-        if (err) throw err;
-        io.emit('mysqlData', result);
-        connection.release();
-        if (err) throw err;
+    connection.query("SELECT * FROM " + config.mysqlTable, function (err, result) {
+      io.emit('mysqlData', result);
+      connection.release();
+      if (err) throw err;
     });
   })
 }
